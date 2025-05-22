6f80aec16a4b48fcdf71610af71dba175032e6cb
Maria <saphess@mail.ru>
2025-05-22 22:11:04 +0300

    added new command line options

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      const device = config.env.device || "desktop";
+
+      if (device === "mobile") {
+        config.viewportWidth = 375;
+        config.viewportHeight = 667;
+      } else if (device === "desktop") {
+        config.viewportWidth = 1366;
+        config.viewportHeight = 768;
+      }
+
+      return config;
+    },
+
     baseUrl: "http://localhost:3000/",
   },
 });
